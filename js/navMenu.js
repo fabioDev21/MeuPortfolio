82d877e3ab32251e9ba36505d03f1e42ee5e60be
peije21 <fabiotwist1@gmail.com>
2024-03-05 00:02:29 -0300

    adicionando troca de cores apenas aos elementos do sobre mim e próximos passos serão dos elementos de projetos e icones de ferramentas

--- js/navMenu.js
+++ js/navMenu.js
@@ -22,8 +22,8 @@ function mostraMenuNav(){
 }
 
 // Definição do light mode da página
-// TODO: adicionar troca cores do texto, elementos bloco do projeto, sombras do icones das ferramentas.
-// TODO: pensar se troco ou não a cor do background linear
+// TODO: adicionar troca cores do texto = feito!, elementos bloco do projeto, sombras do icones das ferramentas.
+// TODO: pensar se troco ou não a cor do background linear = foi trocado!
 let sunMode = document.getElementById("sunMode")
 console.log(sunMode.title)
 
@@ -40,12 +40,38 @@ function ativarTema(){
 }
 
 function temaClaro(){
+    // aplicando cor para cada elemento da classe bloco e cada declaração subsequente dessa forma age da mesma maneira
     Array.from(document.getElementsByClassName("bloco")).forEach(elemento => {
         elemento.style.color = "var(--prime-color)"
         elemento.style.backgroundColor = "var(--second-color)"
     })
+    Array.from(document.getElementsByClassName("navegacao__item")).forEach(elemento => {
+        elemento.style.color = "var(--prime-color)"
+        // método que adiciona hover após substituição feita pelo próprio JS
+            elemento.addEventListener("mouseenter", a => {
+                elemento.style.color = "var(--second-color)"
+                elemento.style.backgroundColor = "var(--prime-color)"
+            })
+            elemento.addEventListener("mouseleave", a => {
+                elemento.style.color = "var(--prime-color)"
+                elemento.style.backgroundColor = "transparent"
+            })
+    })
+    menuIcon.style.color = "var(--prime-color)"
+    menuIcon.addEventListener("mouseenter", a => {
+        menuIcon.style.color = "var(--second-color)"
+        menuIcon.style.backgroundColor = "var(--prime-color)"
+    })
+    menuIcon.addEventListener("mouseleave", a => {
+        menuIcon.style.color = "var(--prime-color)"
+        menuIcon.style.backgroundColor = "transparent"
+    })
+    navegacao.style.backgroundColor = "var(--second-color)"
+    document.getElementById("verMaisBtn").style.color = "var(--prime-color)"
+    document.getElementById("verMaisBtn").style.filter = "drop-shadow(0 0 0.20rem var(--prime-color))"
     document.getElementById("suBody").style.backgroundColor = "var(--prime-color)"
     document.getElementById("suBody").style.color = "var(--second-color)"
+    document.getElementsByTagName("body")[0].style.background = "fixed linear-gradient(-120deg , #2B3A67,  #ededf4)"
 }
 
 function temaEscuro(){
@@ -53,6 +79,30 @@ function temaEscuro(){
         elemento.style.color = "var(--second-color)"
         elemento.style.backgroundColor = "var(--prime-color)"
     })
+    
+    Array.from(document.getElementsByClassName("navegacao__item")).forEach(elemento => {
+        elemento.style.color = "var(--second-color)"
+        elemento.addEventListener("mouseenter", a => {
+            elemento.style.color = "var(--prime-color)"
+            elemento.style.backgroundColor = "var(--second-color)"
+        })
+        elemento.addEventListener("mouseleave", a => {
+            elemento.style.color = "var(--second-color)"
+            elemento.style.backgroundColor = "transparent"
+        })
+    })
+    menuIcon.addEventListener("mouseenter", a => {
+        menuIcon.style.color = "var(--prime-color)"
+        menuIcon.style.backgroundColor = "var(--second-color)"
+    })
+    menuIcon.addEventListener("mouseleave", a => {
+        menuIcon.style.color = "var(--second-color)"
+        menuIcon.style.backgroundColor = "transparent"
+    })
+    menuIcon.style.color = "var(--second-color)"
+    navegacao.style.backgroundColor = "var(--prime-color)"
+    document.getElementById("verMaisBtn").style.color = "var(--second-color)"
     document.getElementById("suBody").style.backgroundColor = "var(--second-color)"
     document.getElementById("suBody").style.color = "var(--prime-color)"
+    document.getElementsByTagName("body")[0].style.background = "fixed linear-gradient(135deg , #191c23,  #65758f)"
 }
